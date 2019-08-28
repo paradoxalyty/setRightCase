e0bf0bd827b4e859689ddf4defd5b13579d55f9c
paradoxalyty <46906762+paradoxalyty@users.noreply.github.com>
2019-08-28 10:16:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,28 @@
-const deposit = prompt("Введите сумму, которую вы хотите положить в банк.");
-checkPrompt(deposit);
+checkPrompt();
 
 /**
  * Функция проверяет введенные данные и в зависимости от полученных данных
  * выдает соответствующую ошибку или вызывает соответствующую функцию для
  * определения правильного падежа
- * @param {string} deposit количество рублей в виде строки.
  * @returns {string} В зависимости от введенных данных - соответствующее предложение..
  */
 function checkPrompt() {
+    const deposit = prompt("Введите сумму, которую вы хотите положить в банк.");
+    
     if (deposit == null) {
+        //Если пользователь нажмет кнопку <<отмена>>, prompt вернет NULL и программа завершит выполнение.
         alert("К сожалетию вы отменили сделку.");
     } else if (isNaN(parseInt(deposit)) || !Number.isInteger(Number(deposit))) {
-        alert("Введите пожалуйста целое число.");
+        alert("Необходимо ввести целое число, попробуйте ещё раз.");
+        checkPrompt();
     } else if (parseInt(deposit) === 0) {
         alert("Вы ввели 0, попробуйте ещё раз.");
+        checkPrompt();
     } else if (parseInt(deposit) < 0) {
         alert("Вы ввели отрицательное число, попробуйте ещё раз.");
+        checkPrompt();
     } else {
-        alert(`Ваша сумма в ${deposit} ${setRightCaseForPenultimateElem()} успешно зачислена.`);
+        alert(`Ваша сумма в ${deposit} ${setRightCaseForPenultimateElem(deposit)} успешно зачислена.`);
     }
 }
 
@@ -26,23 +30,25 @@ function checkPrompt() {
  *Функция возвращает верный падеж для слова "рубль",
  * если предпоследний элемент в строке 'deposit' равен 1 или
  * вызывает следующую функцию для проверки других вариантов.
+ * @param deposit - данные введенные пользователем в prompt.
  * @returns {string} - "рублей".
  */
-function setRightCaseForPenultimateElem() {
+function setRightCaseForPenultimateElem(deposit) {
     let penultimateElem = deposit.charAt(deposit.length - 2);
     if (penultimateElem === '1') {
         return "рублей";
     } else {
-        return setRightCase();
+        return setRightCase(deposit);
     }
 }
 
 /**
  * Функция возвращает верный падеж для слова "рубль", в зависимости
  * от остатка от деления на 10.
+ * @param deposit - данные введенные пользователем в prompt.
  * @returns {string} В зависимости от введенных данных - слово "рубль" в соответствующем падеже .
  */
-function setRightCase() {
+function setRightCase(deposit) {
 
     switch (parseInt(deposit) % 10) {
         case 1:
